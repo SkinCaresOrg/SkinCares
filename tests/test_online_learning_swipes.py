f91a0a9f9d030cfa97b13f9afc089361ac48a16b
sofiagzzloz <sofiagzzloz@gmail.com>
2026-04-08 13:05:38 +0200

    Skip VW-dependent tests when vowpalwabbit is unavailable

--- tests/test_online_learning_swipes.py
+++ tests/test_online_learning_swipes.py
@@ -13,6 +13,11 @@ import numpy as np
 import pandas as pd
 import pytest
 
+from skincarelib.ml_system.online_learning import vowpalwabbit
+
+if vowpalwabbit is None:
+    pytestmark = pytest.mark.skip(reason="vowpalwabbit is not installed")
+
 from skincarelib.ml_system.online_learning import (
     OnlineLearner,
     ContextualBanditStrategy,
